7f484f1600ce4077955921e153977627fcfa2273
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-04-24 22:52:52 +0500

    added support page

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, about
+from .views import home, about, support
 
 
 urlpatterns = [
     path('', home, name='home'),
-    path('about/', about, name='about')
+    path('about/', about, name='about'),
+    path('support/', support, name='support')
 ]
--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,7 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def support(request):
+    return render(request, 'support.html')
